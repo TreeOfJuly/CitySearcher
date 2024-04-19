c72e1e3b02b5a2936e39b0cbb0aaedac281476b8
TreeOfJuly <166689388+TreeOfJuly@users.noreply.github.com>
2024-04-19 16:44:18 -0400

    Create parsingCSV.h

---
+++ parsingCSV.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "hashTable.h"
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <fstream>
+using namespace std;
+
+
+
+
+void parseCSV(hashMapLinear& map){
+   fstream cityInfo;
+   string line, attribute;
+   cityInfo.open("uscities.csv",ios::in);
+   while(!cityInfo.eof()){
+       getline(cityInfo,line);
+       stringstream s(line);
+       vector<string> attributes;
+       while(getline(s,attribute,',')){
+           attributes.push_back(attribute);
+       }
+       map.insert(attributes);
+   }
+   //Done
+}
